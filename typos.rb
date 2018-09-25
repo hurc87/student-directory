23d57fc318445373e5986d72ec908a490e2218f9
hurc87 <zoe1987@btinternet.com>
2018-09-25 12:20:26 +0100

    updated menu

--- typos.rb
+++ typos.rb
@@ -1,20 +1,21 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
   # get another name from the user
   name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
 def print_header
@@ -22,11 +23,11 @@ def print_header
   puts "``-------------``"
 end
 
-def print(students)
-  if students.empty?
+def print_students_list()
+  if @students.empty?
     puts "There is noone on the list"
   else
-    students.each do |student|
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
@@ -36,8 +37,36 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def print_menu
+  puts "1. Input the input_students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll be adding more items
+end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+run interactive_menu
